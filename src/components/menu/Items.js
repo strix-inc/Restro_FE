b2b8e68469aeea54b7da9c1ae5cf864e5afe667f
Aquib479 <mdaquib479@gmail.com>
2023-01-29 16:51:51 +0530

    minor

--- src/components/menu/Items.js
+++ src/components/menu/Items.js
@@ -6,7 +6,7 @@ const Items = ({ Data, mode }) => {
             {
                 Data.map(val => {
                     return val.items.map((item, index) => {
-                        return <div key={index} className={`Item-list grid grid-cols-5 py-1 ${mode === 'black' ? 'text-green-500' : 'text-black'} `}>
+                        return <div key={index} className={`Item-list grid grid-cols-5 py-1 ${mode === 'black' ? 'text-green-500' : 'text-black'} border border-l-0 border-r-0 border-t-0`}>
                             <span className='col-span-2 ml-2'>{item.name}</span>
                             <span className='col-span-1'>
                                 <div>
